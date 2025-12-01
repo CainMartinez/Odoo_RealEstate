bad16e818bcb39418b6bad417170165e72f08227
Cain <cainmartinez2@hotmail.com>
2025-12-01 19:06:09 +0100

    Chapter 7 completed: Add Property Types, Tags, and Offers.

--- addons/real_estate/__manifest__.py
+++ addons/real_estate/__manifest__.py
@@ -15,8 +15,12 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tag_views.xml',
+        'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
         'views/estate_property_actions.xml',
+        'views/estate_property_type_tag_actions.xml',
         'views/estate_menus.xml',
     ],
     'assets': {
