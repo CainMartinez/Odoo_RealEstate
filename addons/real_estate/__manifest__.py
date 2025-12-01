cbbc7396891bc91adc9434568485b4a2fa33bd67
Cain <cainmartinez2@hotmail.com>
2025-12-01 03:50:11 +0100

    Chapter 6 completed: Add estate property views and actions; filters and group by, in the search; (extra) add style with css.

--- addons/real_estate/__manifest__.py
+++ addons/real_estate/__manifest__.py
@@ -16,8 +16,14 @@
     'data': [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
+        'views/estate_property_actions.xml',
         'views/estate_menus.xml',
     ],
+    'assets': {
+        'web.assets_backend': [
+            'real_estate/static/src/css/estate.css',
+        ],
+    },
     'installable': True,
     'application': True,
     'auto_install': False,
