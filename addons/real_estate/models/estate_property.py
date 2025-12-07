4ef155dde7ff93225038531ad851cf0f4c589543
Cain <cainmartinez2@hotmail.com>
2025-12-07 22:23:08 +0100

    Chapter 8: Add computed fields for total area and best price in estate property model; update views to display new fields

--- addons/real_estate/models/estate_property.py
+++ addons/real_estate/models/estate_property.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from dateutil.relativedelta import relativedelta
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class EstateProperty(models.Model):
@@ -50,9 +50,35 @@ class EstateProperty(models.Model):
         default='new'
     )
 
+    # Computed fields
+    total_area = fields.Integer(string="Total Area (sqm)", compute="_compute_total_area")
+    best_price = fields.Float(string="Best Offer", compute="_compute_best_price")
+
     # Relational fields
     property_type_id = fields.Many2one('estate.property.type', string='Property Type')
     buyer_id = fields.Many2one('res.partner', string='Buyer', copy=False)
     seller_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
     tag_ids = fields.Many2many('estate.property.tag', string='Tags')
     offer_ids = fields.One2many('estate.property.offer', 'property_id', string='Offers')
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        """Compute total area as sum of living area and garden area"""
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        """Compute best price as the maximum offer price"""
+        for record in self:
+            record.best_price = max(record.offer_ids.mapped('price')) if record.offer_ids else 0.0
+
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        """Set default values for garden area and orientation when garden is enabled"""
+        if self.garden:
+            self.garden_area = 10
+            self.garden_orientation = 'north'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = None
--- addons/real_estate/models/estate_property_offer.py
+++ addons/real_estate/models/estate_property_offer.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from datetime import timedelta
+from odoo import api, fields, models
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,5 +16,22 @@ class EstatePropertyOffer(models.Model):
         string='Status',
         copy=False
     )
+    validity = fields.Integer(string='Validity (days)', default=7)
+    date_deadline = fields.Date(string='Deadline', compute='_compute_date_deadline', inverse='_inverse_date_deadline')
     partner_id = fields.Many2one('res.partner', string='Partner', required=True)
     property_id = fields.Many2one('estate.property', string='Property', required=True)
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        """Compute deadline as create_date + validity days"""
+        for record in self:
+            # Use create_date if available, otherwise use today
+            create_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.date_deadline = create_date + timedelta(days=record.validity)
+
+    def _inverse_date_deadline(self):
+        """Set validity based on deadline"""
+        for record in self:
+            # Use create_date if available, otherwise use today
+            create_date = record.create_date.date() if record.create_date else fields.Date.today()
+            record.validity = (record.date_deadline - create_date).days
