4732f81c0dd956ad9a0833376a05212eaf0f8d55
Cain <cainmartinez2@hotmail.com>
2025-12-09 16:17:35 +0100

    Chatper 14: Add Kanban view for properties and update action view mode

--- addons/real_estate/__manifest__.py
+++ addons/real_estate/__manifest__.py
@@ -21,6 +21,7 @@
         'views/estate_property_tag_views.xml',
         'views/estate_property_offer_views.xml',
         'views/estate_property_views.xml',
+        'views/estate_property_kanban_views.xml',
         'views/res_users_views.xml',
         'views/estate_menus.xml',
     ],
